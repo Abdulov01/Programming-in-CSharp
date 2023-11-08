7cb9f418dea972a986888966288fd4a76be68ab6
Abdulov01 <117936321+Abdulov01@users.noreply.github.com>
2023-11-09 00:07:12 +0400

    Add

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+
+/*Console.WriteLine("Hello, World!");
+byte a = 6;
+Console.WriteLine("write the number");
+int b = int.Parse(Console.ReadLine());
+int c = a + b;
+
+
+Console.WriteLine("Value = " + c );
+var intMin = int.MinValue;
+*/
+
+
+Console.WriteLine("Input the first letter");
+string Letters1 = Console.ReadLine();
+Console.WriteLine("Input the second letter");
+string Letters2 = Console.ReadLine();
+Console.WriteLine("Input the third letter");
+string Letters3 = Console.ReadLine();
+
+Console.WriteLine("Your reversed letters is:");
+
+Console.WriteLine(Letters3
+    );
+Console.WriteLine(Letters2
+    );
+Console.WriteLine(Letters1
+    );
+
+
