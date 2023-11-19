71da0d13e72023526f616c202440a59a8ccb900c
Abdulov01 <117936321+Abdulov01@users.noreply.github.com>
2023-11-19 22:51:25 +0400

    New Commit

---
+++ LESSON 5/LESSON 5/Program.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main1()
+
+    {
+
+        //Task 1 - Write a program in C# to display the first 10 natural numbers
+
+        Console.WriteLine("Program in C# to display the first 10 natural numbers");
+
+        int[] NaturalNumbers = new int[10];
+        
+
+        for (int i = 1; i <=10; i++)
+        {
+          
+            NaturalNumbers[i-1] = i;
+            Console.WriteLine(i);
+
+
+        }
+
+
+    }
+
+    static void Main2(string[] args)
+    {
+        //Task 2 - Write a C# program to find the sum of first 20 odd natural numbers.
+
+        Console.WriteLine("Program to find the sum of first 20 odd natural numbers");
+        int sum = 0;
+        int value = 20;
+
+        for (int i = 1; i<=value*2; i= i+2)
+        {
+            sum = sum + i;  
+
+        }
+
+        Console.WriteLine($"Sum of first 20 odd natural numbers is: {sum}");
+
+    }
+
+
+    static void Main3()
+    {
+        //Task 3 - Write a program in C# to read 5 numbers from keyboard and find their sum and average.
+
+        Console.WriteLine("Program in C# to read 5 numbers from keyboard and find their sum and average");
+        int [] Numbers = new int[5];
+        int sum = 0;
+        int average = 0;
+
+        try
+        {
+
+
+            Console.WriteLine("Enter the first integer number");
+            Numbers[0] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second integer number");
+            Numbers[1] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third integer number");
+            Numbers[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fourth integer number");
+            Numbers[3] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fifth integer number");
+            Numbers[4] = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                sum = sum + Numbers[i];
+            }
+
+            average = sum / Numbers.Length;
+
+            Console.WriteLine($"Sum is: {sum}");
+            Console.WriteLine($"Average is: {average}");
+        }
+        catch 
+        {
+            Console.WriteLine("Please restart the program and enter the numbers only integer type");
+        }
+
+    }
+
+    static void Main4(string[] args)
+    {
+
+        //Task 4 - Write a program in C# to display the multiplication table vertically from 1 to n.
+
+        Console.WriteLine("Program in C# to display the multiplication table vertically from 1 to n.");
+        int uptoTable;
+        Console.WriteLine("Enter the number for multiplication table");
+        try
+        {
+            uptoTable = int.Parse(Console.ReadLine());
+            Console.WriteLine("THE MULTIPLICATION TABLE: ");
+
+            for (int j = 1; j <= 10; j++)
+            {
+
+                for (int i = 1; i <= uptoTable; i++)
+                {
+                    Console.Write($"{i}*{j}={i * j}, ");
+
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+        catch 
+        {
+            Console.WriteLine("Please restart the program and enter the numbers only integer type");
+        }
+    }
+
+    static void Main5()
+    {
+        //Task 5 - Write a program in C# to display the pattern like right angle triangle using an asterisk.
+
+        Console.WriteLine("Program in C# to display the pattern like right angle triangle using an asterisk");
+        Console.WriteLine("Enter the number for triangle:");
+        int Triangesize = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("THIS IS YOUR TRIANGLE:");
+        
+    for (int i=1; i<=Triangesize; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine() ;
+        }
+
+    }
+
+    static void Main6(string[] args)
+    {
+        //Task 6 - Write a program in C# to make such a pattern like right angle triangle with number increased by 1.
+        Console.WriteLine("program in C# to make such a pattern like right angle triangle with number increased by 1");
+        Console.WriteLine("Enter the number for triangle size:");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int k=1;
+        int j; 
+        for (int i=1; i<=size; i++)
+        {
+            for( j=1; j<=i; j++)
+            {
+              Console.Write(k);
+                k++;
+              
+            }
+           
+            Console.WriteLine() ;
+           
+        }
+      
+    }
+
+
+    static void Main7()
+    {
+        //Task 7 - Write a program in C# to display the sum of the series [ 9 + 99 + 999 + 9999 ...].
+        Console.WriteLine("Program in C# to display the sum of the series [ 9 + 99 + 999 + 9999 ...]");
+        Console.WriteLine("Enter the series size:");
+        int SeriesSize = Convert.ToInt32(Console.ReadLine());
+        int nine = 9;
+        int Sum = 0;
+       
+            for (int i = 1; i <= SeriesSize; i++)
+            {
+            Sum += nine;
+                Console.Write(nine);
+            nine = nine * 10 + 9;
+            Console.Write(" ");
+
+
+             }
+            Console.WriteLine();
+           Console.WriteLine($"The Sum is: {Sum}");
+            
+    }
+       
+       static void Main(string[] args)
+    {
+        //Task 8 - Write a C# program to find prime numbers up to users entered value. 
+
+        Console.WriteLine("Program for to find prime numbers up to users entered value");
+        Console.WriteLine("Enter the upper limit:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        bool isPrime = true;
+        for (var i = 2; i <= num; i++)
+        {
+
+            for (var j = 2; j <= num; j++)
+            {
+                if (i != j && i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.Write(i + ",");
+            }
+            isPrime = true;
+
+        }
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LESSON 6/LESSON 6/Program.cs
@@ -0,0 +1,197 @@
+﻿
+using System.Security.Cryptography;
+using System.Linq;
+
+class Program
+{
+    static void Main1(string[] args)
+    {
+        //Task (1-3) - Enter the value between 10 and 100. Initialize int array with random values with length of user entered value
+
+        int arraySize;
+        Random random = new Random();
+
+        Console.WriteLine("Enter the integer number for array size");
+        arraySize = int.Parse(Console.ReadLine());
+
+        int[] randomArray = new int[arraySize];
+
+        for (int i = 0; i < arraySize; i++)
+        {
+            randomArray[i] = random.Next(10, 100);
+        }
+
+        for (int j = 0; j < arraySize; j++)
+        {
+
+            Console.WriteLine(randomArray[j]);
+        }
+
+        //Task 1 - Write a program in C# sort array both descending and ascending order
+
+        Array.Sort(randomArray);
+        Console.WriteLine("Ascending Array: ");
+        foreach (int k in randomArray)
+        {
+            Console.Write(k + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Descending Array: ");
+        Array.Reverse(randomArray);
+        foreach (int k in randomArray)
+        {
+            Console.Write(k + " ");
+        }
+        Console.WriteLine() ;
+
+
+
+        //Task 2 - Write a C# program to find the 3rd max element of array.
+
+        Console.WriteLine($"The 3rd max element in array is: {randomArray[2]}");
+
+        //Task 3 - Write a program in C# to find first 4 max elements.
+
+        int[] first4Max = new int[4];
+
+        for (int i = 1 ;i <= first4Max.Length; i++)
+        {
+            first4Max[i-1] = randomArray[i-1];
+
+        }
+        Console.WriteLine("First 4 max element in the array are: ");
+        foreach(int k in first4Max)
+        {
+            Console.WriteLine(k + " "); 
+        }
+
+    }
+    static void Main2(string[] args)
+    {
+        //Task 4 - Write a program in C# to create a function to swap the values of two integer numbers without additional variable.
+        //Test Data : Enter a number: 5 Enter another number: 6 Expected Output : Now the 1st number is : 6 , and the 2nd number is : 5
+
+        int a;
+
+        int b;
+
+        Console.WriteLine("Enter the first integer number:");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second integer number:");
+        b = Convert.ToInt32(Console.ReadLine());
+
+
+        (a, b) = (b, a);
+
+        Console.WriteLine($"Now the first number is: {a} The second number is: {b}");
+
+
+    }
+
+
+
+
+
+    //Task 5 - Write a program in C# to create a function to display the n number Fibonacci sequence.
+
+    static void Main3(string[] args)
+    {
+        int n;
+
+        Console.WriteLine("Enter the integer number for fibonachi series: ");
+        n = int.Parse(Console.ReadLine());
+
+        int[] fibonachiNumbers = new int[n];
+
+        fibonachiNumbers[0] = 0;
+        fibonachiNumbers[1] = 1;
+
+        for (int i = 2; i < fibonachiNumbers.Length; i++)
+        {
+            fibonachiNumbers[i] = fibonachiNumbers[i - 1] + fibonachiNumbers[i - 2];
+        }
+        Console.WriteLine("Your fibonachi numbers are: ");
+        foreach (int i in fibonachiNumbers)
+        {
+            Console.Write(i + " ");
+
+        }
+
+    }
+
+
+    static void Main4()
+    {
+        //Task 6 - Write a program in C# to create a function to calculate the sum of the individual digits of a given number
+        int number;
+        int sum = 0;
+        int divideResult;
+        Console.WriteLine("Enter the integer number: ");
+
+        number = Convert.ToInt32(Console.ReadLine());
+
+       while (number > 0)
+        {
+            divideResult = number % 10;
+            sum += divideResult;
+            number = number / 10;
+
+        }
+       
+       Console.WriteLine($"The sum of the digits of the number = {sum}");
+
+    }
+
+    static void Main(string[] args)
+    {
+        //Task 7 - Write a program in C# Sharp to create a function to input a string and count number of spaces are in the string.
+        int count = 0;
+        string data;
+        Console.WriteLine("Program for to count number of spaces are in the string");
+        Console.WriteLine(" ");
+        Console.WriteLine("Enter your string data: ");
+
+        data = Console.ReadLine();
+        
+        for (int i = 0; i<data.Length; i++)
+        {
+            if (data[i] == ' ')
+            {
+                count += 1;
+            }
+
+        }
+
+       Console.WriteLine($"The number of spaces in this text = {count}");
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
