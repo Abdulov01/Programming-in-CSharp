acfd5fdf4fddeccfb77a008a62828f5d671ef69a
Abdulov01 <117936321+Abdulov01@users.noreply.github.com>
2023-11-26 07:03:37 +0400

    Download random image from url

---
+++ LESSON 11/Download_image_from_url/Download_image_from_url/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Text.Json.Nodes;
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        int i;
+        
+        string myUrl = @"https://dog.ceo/api/breeds/image/random";
+        Console.WriteLine("Getting process...");
+
+        for (i = 0; i < 10; i++) 
+        {
+            string adress = @"C:\Users\Abdulov\Downloads\Dogs\" + Guid.NewGuid();
+            HttpClient http = new HttpClient();
+            var result = http.GetStringAsync(myUrl).Result;
+            Url url = JsonConvert.DeserializeObject<Url>(result);
+
+            WebClient web = new WebClient();
+
+            web.DownloadFile(url.message, adress + ".jpg");    
+        }
+
+        Console.WriteLine("The process succesfully finished");
+    }
+}
+    public class Url
+    {
+        public string message { get; set; }
+        public string status { get; set; }
+    }
+
