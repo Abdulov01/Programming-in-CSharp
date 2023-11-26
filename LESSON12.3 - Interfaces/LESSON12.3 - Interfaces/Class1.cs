783cf579be9875c798f9e8c1c6328df780fcb310
Abdulov01 <117936321+Abdulov01@users.noreply.github.com>
2023-11-26 11:01:33 +0400

    Interface example

---
+++ LESSON12.3 - Interfaces/LESSON12.3 - Interfaces/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LESSON12._3___Interfaces
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            if (vehicle.startingGasoline > 0)
+            {
+                vehicle.Drive();
+            }
+            else
+            {
+                Console.WriteLine("You have no fuel. Enter the amount of fuel:");
+                vehicle.Refuel(int.Parse(Console.ReadLine()));
+                Console.WriteLine($"Fuel amount = {vehicle.startingGasoline}");
+                vehicle.Drive();
+            }
+        }
+    }
+    class Vehicle : IVehicle
+    {
+        public int startingGasoline;
+        public Vehicle()
+        {
+            startingGasoline = 0;
+        }
+       public void Drive()
+        {
+            Console.WriteLine("The Car is driving..."); 
+        }
+        public bool Refuel(int gasoline)
+        {
+            bool result;
+            startingGasoline += gasoline;
+
+            if (startingGasoline > 0) 
+            { 
+                return true;
+            }
+            else
+            {
+                return false;
+            }   
+        }
+    }
+}
+
---
+++ LESSON12.3 - Interfaces/LESSON12.3 - Interfaces/Interface1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LESSON12._3___Interfaces
+{
+    public interface IVehicle
+    {
+        void Drive()
+        {
+
+        }
+        bool Refuel(int gasoline)
+        {
+            return false;
+        }
+    }
+}
