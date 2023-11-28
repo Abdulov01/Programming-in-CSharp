1f82e1e630cf9dc87f5c6c5f8dd7c86c57dcc4f8
Abdulov01 <117936321+Abdulov01@users.noreply.github.com>
2023-11-28 13:48:05 +0400

    Timer_for_Shutdown

---
+++ LESSON 13/PC_Shutdown/PC_Shutdown/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading;
+using System;
+using System.Timers;
+using System.Diagnostics;
+
+class Program
+{
+    static System.Timers.Timer timer;
+
+    static void Main()
+    {
+        Console.WriteLine("Enter the time for shutdown PC: (minutes)");
+        TimerSet(int.Parse(Console.ReadLine()));
+    }
+    public static void TimerSet(int minute)
+    {
+        timer = new System.Timers.Timer();
+        timer.Interval = minute * 60000;
+        timer.Elapsed += Shutdown;
+        timer.Start();
+        Console.ReadKey();
+    }
+    public static void Shutdown(object sender, EventArgs e)
+    {
+        Process.Start("shutdown", "/s /t 0");
+        timer.Stop();
+    }
+}
